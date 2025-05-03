d7679daa2f9488d4506ce9f9f4a9aa1a9216b75c
Gaveen Amarasinghe <gaveenamarasinghe2004@gmail.com>
2025-05-03 11:19:24 +0530

    Update App.js

--- eduquest/src/App.js
+++ eduquest/src/App.js
@@ -2,22 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./Components/Login";
 import SignupPage from "./Components/Signup";
-import Header from "./Components/Header";
-import ForgetPassword from "./Components/forgotpassword";
 import ResetPassword from "./Components/resetpassword";
-import AdminPanel from "./Components/admin";
-import Home from "./Components/Home";
-import Footer from "./Components/footer";
-import CoursesPage from "./Components/courses";
-import Services from "./Components/services";
-import AboutUs from "./Components/aboutus";
-import ContactPage from "./Components/contact";
-import PaymentPage from "./Components/payment";
-import ExplorePage from "./Components/explore";
-import MaterialsPage from "./Components/materials";
-import QuizzesPage from "./Components/quizes";
-import VideoPage from "./Components/video";
-import ProgressTracking from "./Components/progress";
+
 
 
 
