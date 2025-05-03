031ed9a1dc41da24be5c7054c98a5271387e8721
Gaveen Amarasinghe <gaveenamarasinghe2004@gmail.com>
2025-05-03 10:06:24 +0530

    Creating reset password page

---
+++ eduquest/src/Components/resetpassword.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate for redirection
+import "./resetpassword.css";
+
+const ResetPassword = () => {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [showPopup, setShowPopup] = useState(false);
+  const navigate = useNavigate(); // Navigation hook
+
+  const validatePassword = (password) => {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return regex.test(password);
+  };
+  
