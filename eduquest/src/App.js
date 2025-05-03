e8ae24602ab9bab8f1d65ec3f9f55b069888573d
Gaveen Amarasinghe <gaveenamarasinghe2004@gmail.com>
2025-05-03 18:37:11 +0530

    Update App.js

--- eduquest/src/App.js
+++ eduquest/src/App.js
@@ -5,7 +5,9 @@ import SignupPage from "./Components/Signup";
 import Header from "./Components/Header";
 import ForgetPassword from "./Components/forgotpassword";
 import ResetPassword from "./Components/resetpassword";
+import Home from "./Components/Home";
 import Footer from "./Components/footer";
+import CoursesPage from "./Components/courses";
 
 
 
@@ -17,11 +19,13 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/header" element={<Header />} />
         <Route path="/footer" element={<Footer />} />
         <Route path="/forgotpassword" element={<ForgetPassword />} />
         <Route path="/resetpassword" element={<ResetPassword />} />
-        
+        <Route path="/courses" element={<CoursesPage />} />
+       
       </Routes>
     </Router>
   );
