1a1ac3d04457578ac0603f194d0e993360451001
Gaveen Amarasinghe <gaveenamarasinghe2004@gmail.com>
2025-05-03 14:58:58 +0530

    Create Fogot Password Page

--- eduquest/src/Components/forgotpassword.jsx
+++ eduquest/src/Components/forgotpassword.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom"; // Import useNavigate
-import "./forgotpassword.css"; // Assuming the CSS file exists for styling
+import { useNavigate } from "react-router-dom"; 
+import "./forgotpassword.css"; 
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
   const [showPopup, setShowPopup] = useState(false);
-  const [isSubmitting, setIsSubmitting] = useState(false); // Track submission status
-  const navigate = useNavigate(); // Initialize useNavigate
-
+  const [isSubmitting, setIsSubmitting] = useState(false); 
+  const navigate = useNavigate();
   
