8d2e6431bd27eb87a2aff48cbf7c176c40451105
Gaveen Amarasinghe <gaveenamarasinghe2004@gmail.com>
2025-05-03 23:10:19 +0530

    Update App.js

--- eduquest/src/App.js
+++ eduquest/src/App.js
@@ -5,11 +5,19 @@ import SignupPage from "./Components/Signup";
 import Header from "./Components/Header";
 import ForgetPassword from "./Components/forgotpassword";
 import ResetPassword from "./Components/resetpassword";
+import AdminPanel from "./Components/admin";
 import Home from "./Components/Home";
 import Footer from "./Components/footer";
 import CoursesPage from "./Components/courses";
+import Services from "./Components/services";
 import AboutUs from "./Components/aboutus";
-
+import ContactPage from "./Components/contact";
+import PaymentPage from "./Components/payment";
+import ExplorePage from "./Components/explore";
+import MaterialsPage from "./Components/materials";
+import QuizzesPage from "./Components/quizes";
+import VideoPage from "./Components/video";
+import ProgressTracking from "./Components/progress";
 
 
 
@@ -25,9 +33,17 @@ function App() {
         <Route path="/footer" element={<Footer />} />
         <Route path="/forgotpassword" element={<ForgetPassword />} />
         <Route path="/resetpassword" element={<ResetPassword />} />
+        <Route path="/admin" element={<AdminPanel />} />
         <Route path="/courses" element={<CoursesPage />} />
+        <Route path="/services" element={<Services />} />
         <Route path="/aboutus" element={<AboutUs />} />
-      
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/explore" element={<ExplorePage />} />
+        <Route path="/material" element={<MaterialsPage />} />
+        <Route path="/quizes" element={<QuizzesPage />} />
+        <Route path="/video" element={<VideoPage />} />
+        <Route path="/progress" element={<ProgressTracking />} />
         
       </Routes>
     </Router>
